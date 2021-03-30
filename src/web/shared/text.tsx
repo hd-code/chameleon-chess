0d1a4a725d91754d974fc69759816166f71bdedb
hd-code <54773293+hd-code@users.noreply.github.com>
2021-03-30 21:59:55 +0200

    - updated dependencies - fixed tests - linter fixes

--- src/web/shared/text.tsx
+++ src/web/shared/text.tsx
@@ -3,6 +3,6 @@ import React, { FC } from 'react';
 // -----------------------------------------------------------------------------
 
 const component: FC = props => {
-    return <>{props.children}</>
+    return <>{props.children}</>;
 };
 export default component;
